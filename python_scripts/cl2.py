8d39f9e4c06406e9f382b260d8a1606987b25f3f
Chris Panagiotakopoulos <admin@server.home>
2016-03-18 18:40:37 +0200

    Get config.js

---
+++ python_scripts/cl2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python           # This is server.py file
+
+import socket
+
+s = socket.socket()
+target = '52.169.181.92'
+port = 80
+s.connect((target,port))
+
+while True:
+	data = s.recv(1024)
+	print 'Received', repr(data)
+s.close()
+
+
